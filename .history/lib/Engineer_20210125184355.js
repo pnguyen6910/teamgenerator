31a846f782597e817155a9159da9e4137f4a6313
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-01-25 18:54:39 -0600

    importing from employee

--- .history/lib/Engineer_20210125184355.js
+++ .history/lib/Engineer_20210125184355.js
@@ -1,16 +1,8 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const Employee = require("./Employee");
+const Letter = require("./Employee");
 
 class Engineer {
     constructor(name, id, email, role, github) {
         this.github = github
-    }
-
-    getRole() {
-        return this.role
-    }
-
-    getGithub() {
-        return this.github
     }
 }
--- .history/lib/Engineer_20210125184507.js
+++ .history/lib/Engineer_20210125184507.js
@@ -1,5 +1,5 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-const Employee = require("./Employee");
+const Letter = require("./Employee");
 
 class Engineer {
     constructor(name, id, email, role, github) {
